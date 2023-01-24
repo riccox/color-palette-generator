9b044d1786b054a87346650a5a338a301ed474bc
Ricco Xie <ricco@riccox.com>
2023-01-24 16:33:50 +0800

    feat: singleColorPalette function extended build: use release-please as release workflow
    Signed-off-by: Ricco Xie <ricco@riccox.com>

--- src/palette.ts
+++ src/palette.ts
@@ -3,13 +3,37 @@ import {HEXColor} from "./index";
 
 /**
  * returns a new hex color string array by given primary color.
- * the primary color would be the middle of the array.
+ * the primary color would be near the middle of the array.
  * @author Ricco Xie
  * @param color primary color string
  * @param numOfShades Optional, number of return shades
  * @return a new hex color string array
  */
 export const singleColorPalette = (color: string, numOfShades: number = 12): HEXColor[] => {
-    return chroma.scale([chroma(color).luminance(0.975), color, chroma(color).luminance(0.015)])
+    return chroma.scale([chroma(color).luminance(0.995), color, chroma(color).luminance(0.015)])
+        .colors(numOfShades, 'hex') as HEXColor[];
+}
+
+/**
+ * returns a new hex color string array by given primary color hue value.
+ * the primary color would be near the middle of the array.
+ * @author Ricco Xie
+ * @param hue primary color hue
+ * @param numOfShades Optional, number of return shades
+ * @return a new hex color string array
+ */
+export const singleColorPaletteViaHue = (hue: number, numOfShades: number = 12): HEXColor[] => {
+    return chroma.scale([chroma.hsl(hue,1,0.995), chroma.hsl(hue,1,0.5), chroma.hsl(hue,1,0.015)])
+        .colors(numOfShades, 'hex') as HEXColor[];
+}
+
+/**
+ * returns a hex color string array from white to black scales.
+ * @author Ricco Xie
+ * @param numOfShades Optional, number of return shades
+ * @return a new hex color string array
+ */
+export const getBlackWhiteColorPalette = (numOfShades: number = 12): HEXColor[] => {
+    return chroma.scale()
         .colors(numOfShades, 'hex') as HEXColor[];
 }
