92d4096cadf6350ac9b02805a6a13f1810d9b22c
Ricco Xie <ricco@riccox.com>
2023-02-12 22:17:30 +0800

    perf: foregroundColor function.

--- src/foreground.ts
+++ src/foreground.ts
@@ -1,17 +1,16 @@
-import {HEXColor} from "./index";
-import chroma from 'chroma-js';
+import { HEXColor } from "./index";
+import chroma from "chroma-js";
 
 /**
- * return a new hex color string (#000 or #FFF) by given background color.
+ * return a new hex color string with high contrast by given background color.
  * @author Ricco Xie
- * @param bgColor background color hex string
- * @return a new hex color string (#000 or #FFF)
+ * @param bgColor background color string
+ * @return a new hex color string
  */
-export const foregroundColor = (bgColor: HEXColor): "#000000" | "#FFFFFF" => {
-    const luma = chroma(bgColor).luminance();
-    const alpha = chroma(bgColor).alpha();
-    if (luma < 0.5 && alpha < 0.5) return '#000000';
-    return luma > 0.5 ? "#000000" : "#FFFFFF";
+export const foregroundColor = (bgColor: string): "#000000" | "#FFFFFF" => {
+  const [r, g, b] = chroma(bgColor).rgb();
+  const sum = Math.round((r * 299 + g * 587 + b * 114) / 1000);
+  return sum > 128 ? "#000000" : "#FFFFFF";
 };
 
 /**
@@ -22,9 +21,9 @@ export const foregroundColor = (bgColor: HEXColor): "#000000" | "#FFFFFF" => {
  * @return a new hex color string
  */
 export const foregroundColorViaLum = (bgColor: HEXColor): HEXColor => {
-    const luma = chroma(bgColor).luminance();
-    const alpha = chroma(bgColor).alpha();
-    let targetLum = luma > 0.5 ? luma - 0.5 : luma + 0.5;
-    if (targetLum > 0.5 && alpha < 0.5) targetLum = 0.4;
-    return chroma(bgColor).luminance(targetLum).hex() as HEXColor;
+  const luma = chroma(bgColor).luminance();
+  const alpha = chroma(bgColor).alpha();
+  let targetLum = luma > 0.5 ? luma - 0.5 : luma + 0.5;
+  if (targetLum > 0.5 && alpha < 0.5) targetLum = 0.4;
+  return chroma(bgColor).luminance(targetLum).hex() as HEXColor;
 };
