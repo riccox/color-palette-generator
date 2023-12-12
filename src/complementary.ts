f8bf74062070250ec2b0182d8f283b6a26c93323
Ricco Xie <ricco@riccox.com>
2023-12-12 22:36:44 +0800

    chore: update author info.
    style: reformat codes.

--- src/complementary.ts
+++ src/complementary.ts
@@ -1,4 +1,4 @@
-import {HEXColor} from "./index";
+import { HEXColor } from "./index";
 
 /**
  * return a new hex complementary color string by given source color.
@@ -7,11 +7,11 @@ import {HEXColor} from "./index";
  * @return a new hex color string
  */
 export const complement = (color: HEXColor): HEXColor => {
-    const colorPart = color.slice(1);
-    const ind = parseInt(colorPart, 16);
-    let iter = ((1 << 4 * colorPart.length) - 1 - ind).toString(16);
-    while (iter.length < colorPart.length) {
-        iter = '0' + iter;
-    }
-    return `#${iter}`;
+  const colorPart = color.slice(1);
+  const ind = parseInt(colorPart, 16);
+  let iter = ((1 << (4 * colorPart.length)) - 1 - ind).toString(16);
+  while (iter.length < colorPart.length) {
+    iter = "0" + iter;
+  }
+  return `#${iter}`;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type HEXColor = `#${string}`;
 
-export * from './palette'
-export * from './foreground'
-export * from './complementary'
+export * from "./palette";
+export * from "./foreground";
+export * from "./complementary";
